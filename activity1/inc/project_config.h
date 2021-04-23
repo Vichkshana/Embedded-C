c1409cabd3cb44e80e2c0f6c0b6a1806f74b862c
Vichkshana <80635930+Vichkshana@users.noreply.github.com>
2021-04-23 22:21:51 +0530

    Update project_config.h

--- activity1/inc/project_config.h
+++ activity1/inc/project_config.h
@@ -2,7 +2,7 @@
 #define __PROJECT_CONFIG_H__
 /**
  * @file project_config.h
- * @author Bharath.G ()
+ * @author Vichkshana ()
  * @brief Configuration file to define pins and ports for the interfaced peripherals
  * @version 0.1
  * @date 2021-04-23
